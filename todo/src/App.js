e5f56c5fe57eebb01ba75a8238ce1a843e5ce12b
William <wtpwinberg@gmail.com>
2018-03-19 18:05:33 -0700

    Initilized app

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [];
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To Do List:</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
